329c6746d15708f9b55ff9b0cf1e1e430d8f5bcd
alexxxevd <alexxxevd@gmail.com>
2024-11-14 23:22:33 +0300

    new

--- src/Cookie.java
+++ src/Cookie.java
@@ -1,5 +1,8 @@
 public class Cookie {
     public static void bike() {
         System.out.println("Pechen'ka");
+        System.out.println("Pechen'ka");
+        System.out.println("Pechen'ka");
+
     }
 }
